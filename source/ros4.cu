78d47d280a5adcb05a9d2ad471171dd09f2ab059
Michail Alvanos <malvanos@gmail.com>
2017-04-06 15:24:59 +0300

    Added ros4 specialized solver.

---
+++ source/ros4.cu
@@ -0,0 +1,374 @@
+
+
+__device__  static  int ros_Integrator_ros4(double * __restrict__ var, const double * __restrict__ fix, const double Tstart, const double Tend, double &T,
+        //  Rosenbrock method coefficients
+        const double * __restrict__ ros_M, const double * __restrict__ ros_E, const double * __restrict__ ros_A, const double * __restrict__  ros_C, 
+        const double * __restrict__ ros_Alpha, const double * __restrict__ ros_Gamma, const double ros_ELO, const int * ros_NewF, 
+        //  Integration parameters
+        const int autonomous, const int vectorTol, const int Max_no_steps, 
+        const double roundoff, const double Hmin, const double Hmax, const double Hstart, double &Hexit, 
+        const double FacMin, const double FacMax, const double FacRej, const double FacSafe, 
+        //  Status parameters
+        int &Nfun, int &Njac, int &Nstp, int &Nacc, int &Nrej, int &Ndec, int &Nsol, int &Nsng,
+        //  cuda global mem buffers              
+        const double * __restrict__ rconst,  const double * __restrict__ absTol, const double * __restrict__ relTol, double * __restrict__ varNew, double * __restrict__ Fcn0, 
+        double * __restrict__ K, double * __restrict__ dFdT, double * __restrict__ jac0, double * __restrict__ Ghimj, double * __restrict__ varErr,
+        // VL_GLO
+        const int VL_GLO)
+{
+    int index = blockIdx.x*blockDim.x+threadIdx.x;
+
+    double H, Hnew, HC, HG, Fac; // Tau - not used
+    double Err; //*varErr;
+    int direction;
+    int rejectLastH, rejectMoreH;
+    const double DELTAMIN = 1.0E-5;
+
+    const int ros_S = 4;
+          
+    //   ~~~>  Initial preparations
+    T = Tstart;
+    Hexit = 0.0;
+    H = fmin(Hstart,Hmax);
+    if (fabs(H) <= 10.0*roundoff) 
+        H = DELTAMIN;
+
+    if (Tend  >=  Tstart)
+    {
+        direction = + 1;
+    }
+    else
+    {
+        direction = - 1;
+    }
+
+    rejectLastH=0;
+    rejectMoreH=0;
+
+
+    //   ~~~> Time loop begins below
+
+    // TimeLoop: 
+    while((direction > 0) && ((T- Tend)+ roundoff <= ZERO) || (direction < 0) && ((Tend-T)+ roundoff <= ZERO))
+    {
+        if (Nstp > Max_no_steps) //  Too many steps
+            return -6;
+        //  Step size too small
+        if (H <= roundoff){  //  Step size too small
+            //if (((T+ 0.1*H) == T) || (H <= roundoff)) {
+            return -7;
+        }
+
+        //   ~~~>  Limit H if necessary to avoid going beyond Tend
+        Hexit = H;
+        H = fmin(H,fabs(Tend-T));
+
+        //   ~~~>   Compute the function at current time
+        Fun(var, fix, rconst, Fcn0, Nfun, VL_GLO);	/// VAR READ - Fcn0 Write
+
+        //   ~~~>  Compute the function derivative with respect to T
+        if (!autonomous)
+            ros_FunTimeDerivative(T, roundoff, var, fix, rconst, dFdT, Fcn0, Nfun, VL_GLO); /// VAR READ - fcn0 read
+
+        //   ~~~>   Compute the Jacobian at current time
+        Jac_sp(var, fix, rconst, jac0, Njac, VL_GLO);   /// VAR READ 
+
+        //   ~~~>  Repeat step calculation until current step accepted
+        // UntilAccepted: 
+        while(1)
+        {
+            ros_PrepareMatrix(H, direction, ros_Gamma[0], jac0, Ghimj, Nsng, Ndec, VL_GLO);
+            //   ~~~>   Compute the stages
+            // Stage: 
+            //for (int istage=0; istage < ros_S; istage++)
+            {
+                int istage = 0;
+		for (int i=0; i<NVAR; i++)		
+			K(index,istage,i)  = Fcn0(index,i);
+
+                if ((!autonomous) && (ros_Gamma[istage] ))
+                {
+                    HG = direction*H*ros_Gamma[istage];
+                    for (int i=0; i<NVAR; i++){
+                        K(index,istage,i) += dFdT(index,i)*HG;
+		     }
+                }
+                ros_Solve(Ghimj, K, Nsol, istage, ros_S);
+            } // Stage
+
+
+            {
+                int istage=1;
+                for (int i=0; i<NVAR; i++){		
+                    varNew(index,i) = K(index,0,i)*ros_A[0]  + var(index,i);
+                }
+
+                Fun(varNew, fix, rconst, varNew, Nfun,VL_GLO); // FCN <- varNew / not overlap 
+
+                HC = ros_C[0]/(direction*H);
+                for (int i=0; i<NVAR; i++){
+                    K(index,1,i) = K(index,0,i)*HC + varNew(index,i);
+                }
+
+                if ((!autonomous))
+                {
+                    HG = direction*H*ros_Gamma[istage];
+                    for (int i=0; i<NVAR; i++){
+                        K(index,istage,i) += dFdT(index,i)*HG;
+		     }
+                }
+                ros_Solve(Ghimj, K, Nsol, istage, ros_S);
+            } // Stage
+
+
+            {
+                int istage=2;
+
+                for (int i=0; i<NVAR; i++){		
+                    varNew(index,i) = K(index,0,i)*0.1867943637803922E+01  + var(index,i);
+                    varNew(index,i) = K(index,1,i)*0.2344449711399156E+00  + varNew(index,i);
+                }
+                Fun(varNew, fix, rconst, varNew, Nfun,VL_GLO); // FCN <- varNew / not overlap 
+
+
+                double HC0 = 0.2580708087951457E+01/(direction*H);
+                double HC1 = 0.6515950076447975E+00/(direction*H);
+
+                for (int i=0; i<NVAR; i++){		
+                    K(index,istage,i)  = varNew(index,i);
+                    K(index,istage,i) += K(index,0,i)*HC0;
+                    K(index,istage,i) += K(index,1,i)*HC1;
+                }
+
+                if ((!autonomous) )
+                {
+                    HG = direction*H*ros_Gamma[2];
+                    for (int i=0; i<NVAR; i++){
+                        K(index,istage,i) += dFdT(index,i)*HG;
+		     }
+                }
+                ros_Solve(Ghimj, K, Nsol, istage, ros_S);
+            } // Stage
+
+       
+            {
+                int istage=3;
+
+		double HC0 = (-0.2137148994382534E+01)/(direction*H);
+		double HC1 = (-0.3214669691237626E+00)/(direction*H);
+		double HC2 = (-0.6949742501781779E+00)/(direction*H);
+
+                for (int i=0; i<NVAR; i++){	
+                    K(index,istage,i)  = varNew(index,i);
+                    K(index,istage,i) += K(index,0,i)*HC0;
+                    K(index,istage,i) += K(index,1,i)*HC1;
+                    K(index,istage,i) += K(index,2,i)*HC2;
+                }
+
+                if ((!autonomous) )
+                {
+                    HG = direction*H*ros_Gamma[istage];
+                    for (int i=0; i<NVAR; i++){
+                        K(index,istage,i) += dFdT(index,i)*HG;
+		     }
+                }
+                ros_Solve(Ghimj, K, Nsol, istage, ros_S);
+            } // Stage
+
+
+
+        //{ -0.2815431932141155E+00, -0.7276199124938920E-01, -0.1082196201495311E+00, -0.1093502252409163E+01, 0, 0}, /* ros_E */
+
+            //  ~~~>  Compute the new solution
+	    for (int i=0; i<NVAR; i++){
+		    double tmpNew ;
+		    double tmpErr ;
+
+                    tmpNew  = K(index,0,i)*(0.2255570073418735E+01) + var(index,i);
+                    tmpNew += K(index,1,i)*(0.2870493262186792E+00);
+                    tmpNew += K(index,2,i)*(0.4353179431840180E+00);
+                    tmpNew += K(index,3,i)*(0.1093502252409163E+01);
+
+                    tmpErr  = K(index,0,i)*(-0.2815431932141155E+00);
+                    tmpErr += K(index,1,i)*(-0.7276199124938920E-01);
+                    tmpErr += K(index,2,i)*(-0.1082196201495311E+00);
+                    tmpErr += K(index,3,i)*(-0.1093502252409163E+01);
+
+		    varNew(index,i) = tmpNew;			// varNew is killed
+		    varErr(index,i) = tmpErr;
+	    }
+
+            Err = ros_ErrorNorm(var, varNew, varErr, absTol, relTol, vectorTol);   /// VAR-varNew READ
+
+
+//  ~~~> New step size is bounded by FacMin <= Hnew/H <= FacMax
+            Fac  = fmin(FacMax,fmax(FacMin,FacSafe/pow(Err,ONE/ros_ELO)));
+            Hnew = H*Fac;
+
+//  ~~~>  Check the error magnitude and adjust step size
+            Nstp = Nstp+ 1;
+            if((Err <= ONE) || (H <= Hmin)) // ~~~> Accept step
+            {
+                Nacc = Nacc + 1;
+                for (int j=0; j<NVAR ; j++)
+                    var(index,j) =  fmax(varNew(index,j),ZERO);  /////////// VAR WRITE - last VarNew read
+
+                T = T +  direction*H;
+                Hnew = fmax(Hmin,fmin(Hnew,Hmax));
+                if (rejectLastH)   // No step size increase after a rejected step
+                    Hnew = fmin(Hnew,H);
+                rejectLastH = 0;
+                rejectMoreH = 0;
+                H = Hnew;
+
+            	break;  //  EXIT THE LOOP: WHILE STEP NOT ACCEPTED
+            }
+            else      // ~~~> Reject step
+            {
+                if (rejectMoreH)
+                    Hnew = H*FacRej;
+                rejectMoreH = rejectLastH;
+                rejectLastH = 1;
+                H = Hnew;
+                if (Nacc >= 1)
+                    Nrej += 1;
+            } //  Err <= 1
+        } // UntilAccepted
+    } // TimeLoop
+//  ~~~> Succesful exit
+    return 0; //  ~~~> The integration was successful
+}
+
+
+__global__ 
+void Rosenbrock_ros4(double * __restrict__ conc, const double Tstart, const double Tend, double * __restrict__ rstatus, int * __restrict__ istatus,
+                // values calculated from icntrl and rcntrl at host
+                const int autonomous, const int vectorTol, const int UplimTol,  const int Max_no_steps,
+                const double Hmin, const double Hmax, const double Hstart, const double FacMin, const double FacMax, const double FacRej, const double FacSafe, const double roundoff,
+                //  cuda global mem buffers              
+                const double * __restrict__ absTol, const double * __restrict__ relTol,
+                // extra
+                const int VL_GLO)
+{
+    int index = blockIdx.x*blockDim.x+threadIdx.x;
+
+    /* Temporary arrays allocated in stack */
+
+    /* 
+     *  Optimization NOTE: runs faster on Tesla/Fermi 
+     *  when tempallocated on stack instead of heap.
+     *  In theory someone can aggregate accesses together,
+     *  however due to algorithm, threads access 
+     *  different parts of memory, making it harder to
+     *  optimize accesses. 
+     *
+     */
+    double varNew_stack[NVAR];
+    double var_stack[NVAR];
+    double varErr_stack[NVAR];
+    double fix_stack[NFIX];
+    double Fcn0_stack[NVAR];
+    double jac0_stack[LU_NONZERO];
+    double dFdT_stack[NVAR];
+    double Ghimj_stack[LU_NONZERO*3];
+    double K_stack[6*NVAR];
+
+    const int method = 3;
+
+    /* Allocated in Global mem */
+    double *rconst = rconst_local;
+
+    /* Allocated in stack */
+    double *Ghimj  = Ghimj_stack;
+    double *K      = K_stack;
+    double *varNew = varNew_stack;
+    double *Fcn0   = Fcn0_stack;
+    double *dFdT   = dFdT_stack;
+    double *jac0   = jac0_stack;
+    double *varErr = varErr_stack;
+    double *var    = var_stack;
+    double *fix    = fix_stack;  
+
+    if (index < VL_GLO)
+    {
+
+        int Nfun,Njac,Nstp,Nacc,Nrej,Ndec,Nsol,Nsng;
+        double Texit, Hexit;
+
+        Nfun = 0;
+        Njac = 0;
+        Nstp = 0;
+        Nacc = 0;
+        Nrej = 0;
+        Ndec = 0;
+        Nsol = 0;
+        Nsng = 0;
+
+        /* FIXME: add check for method */
+        const double *ros_A     = &ros[method-1].ros_A[0]; 
+        const double *ros_C     = &ros[method-1].ros_C[0];
+        const double *ros_M     = &ros[method-1].ros_M[0]; 
+        const double *ros_E     = &ros[method-1].ros_E[0];
+        const double *ros_Alpha = &ros[method-1].ros_Alpha[0]; 
+        const double *ros_Gamma = &ros[method-1].ros_Gamma[0]; 
+        const int    *ros_NewF  = &ros[method-1].ros_NewF[0];
+        const int     ros_S     =  ros[method-1].ros_S; 
+        const double  ros_ELO   =  ros[method-1].ros_ELO; 
+
+        /* Copy data from global memory to temporary array */
+        /*
+         * Optimization note: if we ever have enough constant
+         * memory, we could use it for storing the data.
+         * In current architectures if we use constant memory
+         * only a few threads will be able to run on the fly.
+         *
+         */
+        for (int i=0; i<NVAR; i++)
+            var(index,i) = conc(index,i);
+
+        for (int i=0; i<NFIX; i++)
+            fix(index,i) = conc(index,NVAR+i);
+
+        /* 
+         * Optimization TODO: create versions of the ros_integrator.
+         * Initial experiments show a 15-20% performance gain 
+         * using specialized version.
+         *
+         */
+        ros_Integrator_ros4(var, fix, Tstart, Tend, Texit,
+                //  Rosenbrock method coefficients
+                ros_M, ros_E, ros_A, ros_C, 
+                ros_Alpha, ros_Gamma, ros_ELO, ros_NewF, 
+                //  Integration parameters
+                autonomous, vectorTol, Max_no_steps, 
+                roundoff, Hmin, Hmax, Hstart, Hexit, 
+                FacMin, FacMax, FacRej, FacSafe,
+                //  Status parameters
+                Nfun, Njac, Nstp, Nacc, Nrej, Ndec, Nsol, Nsng,
+                //  cuda global mem buffers              
+                rconst, absTol, relTol, varNew, Fcn0,  
+                K, dFdT, jac0, Ghimj,  varErr, VL_GLO
+                );
+
+        for (int i=0; i<NVAR; i++)
+            conc(index,i) = var(index,i); 
+
+
+        /* Statistics */
+        istatus(index,ifun) = Nfun;
+        istatus(index,ijac) = Njac;
+        istatus(index,istp) = Nstp;
+        istatus(index,iacc) = Nacc;
+        istatus(index,irej) = Nrej;
+        istatus(index,idec) = Ndec;
+        istatus(index,isol) = Nsol;
+        istatus(index,isng) = Nsng;
+        // Last T and H
+        rstatus(index,itexit) = Texit;
+        rstatus(index,ihexit) = Hexit; 
+    }
+}
+
+
+
+
