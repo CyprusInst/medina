456ce149de52ea3c180d173dd7c4c1ca0acbf3b6
Michail Alvanos <malvanos@gmail.com>
2017-05-08 16:02:07 +0300

    Support for complex LU_DIAG with more than 10 tables.

--- f2c_alpha.py
+++ f2c_alpha.py
@@ -641,7 +641,7 @@ def find_LU_DIAG(file_in, NVAR):
     lu_diag = re.sub(r"dimension\([0-9]+\)::lu_diag_[0-9]\s?=\s?\(\/",r",",lu_diag)
     lu_diag = re.sub(r"dimension\([0-9]+\)", r"",lu_diag)
     lu_diag = re.sub(r"::", r"",lu_diag)
-    lu_diag = re.sub(r"lu_diag_[0-9]\s?=\s?",r"",lu_diag)
+    lu_diag = re.sub(r"lu_diag_[0-9]+\s?=\s?",r"",lu_diag)
     lu_diag = re.sub(r"\(/",r"",lu_diag)
     lu_diag = lu_diag.replace("/)\ninteger","")
     lu_diag = lu_diag.replace("parameter,","")
